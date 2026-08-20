e1d8040cad229a6fcf70825c284b72417f1e5644
Lee Jacobs <alexisleejacobs18@gmail.com>
2026-08-19 22:31:59 -0400

    Add VPC flow logs

---
+++ terraform/modules/aws-networking/flow-logs.tf
@@ -0,0 +1,63 @@
+resource "aws_cloudwatch_log_group" "vpc_flow_logs" {
+  name              = "/aws/vpc-flow-logs/${var.environment}"
+  retention_in_days = 30
+
+  tags = merge(var.tags, {
+    Name = "${var.environment}-vpc-flow-logs"
+  })
+}
+
+data "aws_iam_policy_document" "flow_logs_trust" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["vpc-flow-logs.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "flow_logs" {
+  name               = "vpc-flow-logs-${var.environment}"
+  assume_role_policy = data.aws_iam_policy_document.flow_logs_trust.json
+
+  tags = merge(var.tags, {
+    Name = "vpc-flow-logs-${var.environment}"
+  })
+}
+
+data "aws_iam_policy_document" "flow_logs_delivery" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "logs:DescribeLogGroups",
+      "logs:DescribeLogStreams",
+    ]
+    resources = [
+      aws_cloudwatch_log_group.vpc_flow_logs.arn,
+      "${aws_cloudwatch_log_group.vpc_flow_logs.arn}:*",
+    ]
+  }
+}
+
+resource "aws_iam_role_policy" "flow_logs_delivery" {
+  name   = "delivery"
+  role   = aws_iam_role.flow_logs.id
+  policy = data.aws_iam_policy_document.flow_logs_delivery.json
+}
+
+resource "aws_flow_log" "main" {
+  vpc_id                = aws_vpc.main.id
+  traffic_type           = "ALL"
+  log_destination_type   = "cloud-watch-logs"
+  log_destination        = aws_cloudwatch_log_group.vpc_flow_logs.arn
+  iam_role_arn            = aws_iam_role.flow_logs.arn
+
+  tags = merge(var.tags, {
+    Name = "${var.environment}-vpc-flow-log"
+  })
+}
