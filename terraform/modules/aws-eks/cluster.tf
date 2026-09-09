0dc094c10d376c0472d6983377ec055f762f0259
Lee Jacobs <alexisleejacobs18@gmail.com>
2026-09-09 02:20:33 -0400

    Upgrade EKS control plane to 1.36

--- terraform/modules/aws-eks/cluster.tf
+++ terraform/modules/aws-eks/cluster.tf
@@ -1,7 +1,7 @@
 resource "aws_eks_cluster" "this" {
   name     = "dev"
   role_arn = aws_iam_role.cluster.arn
-  version  = "1.35"
+  version  = "1.36"
 
   vpc_config {
     subnet_ids              = var.private_app_subnet_ids
