6cdd425bf43a8b7def74c64981a51a1e5dec52a3
Lee Jacobs <alexisleejacobs18@gmail.com>
2026-09-10 14:45:51 -0400

    Bring EKS control plane CloudWatch log group under Terraform with 30-day retention

---
+++ terraform/modules/aws-eks/logging.tf
@@ -0,0 +1,11 @@
+import {
+  to = aws_cloudwatch_log_group.cluster
+  id = "/aws/eks/dev/cluster"
+}
+
+resource "aws_cloudwatch_log_group" "cluster" {
+  name              = "/aws/eks/dev/cluster"
+  retention_in_days = 30
+
+  tags = merge(var.tags, { Name = "eks-dev-cluster-logs" })
+}
